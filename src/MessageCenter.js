12e34107347f940b8be03998307112b2b97f7883
Ashley Chang <ashley_e_chang@brown.edu>
2020-11-24 04:36:32 -0500

    txt download functionality added

--- src/MessageCenter.js
+++ src/MessageCenter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ls from 'local-storage';
  import { Editor } from '@tinymce/tinymce-react';
 import { Helmet } from 'react-helmet';
+import './index.css'
 
 class NewTitle extends React.PureComponent {
   render () {
@@ -18,54 +19,72 @@ class NewTitle extends React.PureComponent {
 
 class TextEditor extends React.Component {
 
-  constructor(props) {
+   render() {
+     return (
+       <>
+       <Editor
+         initialValue={this.props.text_content}
+         init={{
+           height: 500,
+           menubar: false,
+           plugins: [
+             'advlist autolink lists link image charmap print preview anchor',
+             'searchreplace visualblocks code',
+             'insertdatetime media table paste code help wordcount'
+           ],
+           toolbar:
+             'undo redo | formatselect | bold italic forecolor backcolor | \
+             alignleft aligncenter alignright alignjustify | \
+             bullist numlist outdent indent| help'
+         }}
+         onEditorChange={this.props.handleEditorChange}
+       />
+       </>
+     );
+   }
+ }
+
+ class MessageCenter extends React.Component {
+    constructor(props) {
     super(props);
     this.state = {
       text_content: ls.get('message') || ""
     }
   }
    handleEditorChange = (content, editor) => {
-     console.log('Content was updated:', content);
      this.setState({text_content: content})
      ls.set('message', content);
-     console.log(ls.get('message'))
    }
-
    render() {
-    const init_content = this.state.text_content
-     return (
-       <Editor
-         initialValue={init_content}
-         init={{
-           height: 500,
-           menubar: false,
-           plugins: [
-             'advlist autolink lists link image charmap print preview anchor',
-             'searchreplace visualblocks code fullscreen',
-             'insertdatetime media table paste code help wordcount'
-           ],
-           toolbar:
-             'undo redo | formatselect | bold italic backcolor | \
-             alignleft aligncenter alignright alignjustify | \
-             bullist numlist outdent indent | removeformat | help'
-         }}
-         onEditorChange={this.handleEditorChange}
-       />
-     );
-   }
- }
-
- class MessageCenter extends React.Component {
-   render() {
+    const content = this.state.text_content
+    const { htmlToText } = require('html-to-text');
+    const text = htmlToText(content, {
+      wordwrap: 130
+    });
+    let blob = new Blob([text], {type: "application/txt"});
+    const newLink = window.URL.createObjectURL(blob)
+    const currDay = new Date().getDay().toString()
+    const Day = new Date().toISOString().slice(0,10).replaceAll(" ", "-")
+    const fileName = "note-to-self-" + Day + ".txt"
      return (
          <div className="container">
-         <NewTitle />
          <div>
+         <NewTitle />
+
          <h3 className="title" style={{width: 500, textAlign: "center"}}>
                   Notes To Self
          </h3>
-          <TextEditor />
-          </div>
+          <TextEditor
+              text_content = {this.state.text_content}
+              handleEditorChange = {this.handleEditorChange.bind(this)}
+          />
+           <div className={"note"}>
+               <div style={{display: 'flex'}}>
+               <a href={newLink} download={fileName}><button className={"submit"} style ={{maxWidth: '6rem', marginRight: '1rem'}}>Download</button></a>
+               {/*<a href={newLink} download><button className={"submit"} style ={{minWidth: '4rem'}}>Save</button></a>*/}
+               </div>
+           </div>
+            </div>
           </div>
 
      );
